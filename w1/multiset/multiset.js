a506a48cf0ba129b88350b6d2b7b8715f6c72656
Benjamin Nørgaard <benjamin@webtoad.dk>
2014-11-10 13:40:52 +0100

    Simple multiset implementation

---
+++ w1/multiset/multiset.js
@@ -0,0 +1,86 @@
+document.addEventListener("DOMContentLoaded", function() {
+  function multiset() {
+    
+    this.entries = {};
+
+    function entry(name, value) {
+      this.name = name;
+      this.value = value;
+    }
+
+    this.add = function(o) {
+      var h = o.hashCode();
+      if (this.entries[h]) {
+        this.entries[h].value++;
+      } else {
+        var e = new entry(o, 1);
+        this.entries[h] = e;
+      }
+    }
+
+    this.remove = function(o) {
+      var h = o.hashCode();
+      if (this.entries[h]) {
+        this.entries[h].value--;
+        if (this.entries[h].value == 0) {
+          delete this.entries[h];
+        }
+      }
+    }
+
+    this.count = function(o) {
+      var h = o.hashCode();
+      return this.entries[h].value || 0
+    }
+
+    this.contains = function(o) {
+      var h = o.hashCode();
+      return !!this.entries[h];
+    }
+
+    this.toString = function() {
+      var i = "  "
+        var res = "{\n"
+        for (var p in this.entries) {
+          if (this.entries.hasOwnProperty(p) && this.entries[p].value != 0) {
+            res += i + this.entries[p].name + ": " + this.entries[p].value + "\n"
+          }
+        }
+      return res + "}\n"
+    }
+  }
+
+  String.prototype.hashCode = function() {
+    var hash = 0, i, chr, len;
+    if (this.length == 0) return hash;
+    for (i = 0, len = this.length; i < len; i++) {
+      chr   = this.charCodeAt(i);
+      hash  = ((hash << 5) - hash) + chr;
+      hash |= 0; // Convert to 32bit integer
+    }
+    return hash;
+  }
+
+  var set = new multiset();
+
+  set.add("lol");
+  set.add("lol");
+  set.add("test");
+  var add1 = document.getElementById("add1");
+  add1.innerHTML = set.toString();
+  
+  set.remove("lol");
+  set.remove("test");
+  var remove1 = document.getElementById("remove1");
+  remove1.innerHTML = set.toString();
+
+  set.add("cheese");
+  set.add("cheese");
+  var c = document.getElementById("count");
+  c.innerHTML = set.count("cheese") + "\n" + set.count("lol");
+
+  var cont = document.getElementById("cont");
+  cont.innerHTML = set.contains("cheese") + "\n" + set.contains("lol") + "\n" + set.contains("test");
+  console.log(set.toString());
+  
+});
